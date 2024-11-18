631c2a42fe881f8bf3d70fa0d4a6fbf63c5d768e
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-11-18 23:32:12 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- C++/프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
+++ C++/프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 int solution(int a, int b) {
-    string stra = to_string(a);
-    string strb = to_string(b);
-    stra += strb;
-    int answer = stoi(stra);
-    return answer>2*a*b ? answer : 2*a*b;
+    string s = to_string(a) + to_string(b); 
+    return max(stoi(s), 2 * a * b);
 }
