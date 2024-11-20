e9fd37ed521c6ca70b7061101495be44cbb85f60
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-11-20 20:02:16 +0900

    Time: 3 ms (83.01%) | Memory: 16.6 MB (41.94%) - LeetSync

--- LeetCode/36-valid-sudoku/valid-sudoku.py
+++ LeetCode/36-valid-sudoku/valid-sudoku.py
@@ -1,29 +1,21 @@
 from collections import Counter
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(9):
-            cnt = Counter()
-            for x in board[i]:
-                if x != ".": cnt[x] += 1
-            for v in cnt.values():
-                if v>1:
-                    return False
+        row_cnt = [set() for _ in range(9)]
+        col_cnt = [set() for _ in range(9)]
+        box_cnt = [set() for _ in range(9)]
 
-            cnt = Counter()
-            for y in [board[j][i] for j in range(9)]:
-                for x in y :
-                    if x != ".": cnt[x] += 1
-            for v in cnt.values():
-                if v>1:
-                    return False
 
-        for i in range(0,9,3):
-            for j in range(0,9,3):
-                cnt = Counter()
-                for y in [k[j:j+3] for k in board[i:i+3]]:
-                    for x in y:
-                        if x != ".": cnt[x] += 1
-                for v in cnt.values():
-                    if v>1:
-                        return False
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == ".":
+                    continue
+                val = board[row][col]
+                box_idx = (row//3)*3+(col//3)
+                if val in row_cnt[row] or val in col_cnt[col] or val in box_cnt[box_idx]:
+                    return False
+                row_cnt[row].add(val)
+                col_cnt[col].add(val)
+                box_cnt[box_idx].add(val)
+
         return True
