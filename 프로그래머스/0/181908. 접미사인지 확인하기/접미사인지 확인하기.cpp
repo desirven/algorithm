8b43dc081ba45173bfd4d165d16e795da03c0fd0
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-11-22 21:19:17 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 int solution(string my_string, string is_suffix) {
-    for (int i=0; i<my_string.size(); i++)
-        if (is_suffix.compare(my_string.substr(i))==0)
-            return 1;
-    return 0;
+    if (my_string.size() < is_suffix.size())
+        return 0;
+    return my_string.substr(my_string.size()-is_suffix.size()) == is_suffix;
 }
