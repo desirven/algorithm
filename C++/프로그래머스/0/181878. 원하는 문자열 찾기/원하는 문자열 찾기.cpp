b349b84f71403bc707115582279279f518622571
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-11-27 22:43:35 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.30 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+    transform(pat.begin(), pat.end(), pat.begin(), ::tolower);
+    
+    if (myString.find(pat) != string::npos) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
