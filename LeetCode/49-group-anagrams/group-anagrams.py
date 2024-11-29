e6b81fc3678ea6d37d2885f963ef381d66afd685
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-11-29 21:22:01 +0900

    Time: 11 ms (79.84%) | Memory: 20.1 MB (53.83%) - LeetSync

---
+++ LeetCode/49-group-anagrams/group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        str_map = defaultdict(list)
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            str_map[sorted_s].append(s)
+        return list(str_map.values())
