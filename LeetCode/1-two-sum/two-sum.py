994fb00aa8d2cb8ff922e52e43c63787c4442de6
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-11-30 11:29:58 +0900

    Time: 3 ms (54.11%) | Memory: 18 MB (11.22%) - LeetSync

--- LeetCode/1-two-sum/two-sum.py
+++ LeetCode/1-two-sum/two-sum.py
@@ -1,6 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted_nums = sorted(nums)
+        l, r = 0, len(nums)-1
+        while True:
+            if sorted_nums[l]+sorted_nums[r] == target:
+                break
+            elif sorted_nums[l]+sorted_nums[r] > target:
+                r -= 1
+            else:
+                l += 1
+
+        answer = []
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+            if nums[i] == sorted_nums[l] or nums[i] == sorted_nums[r]:
+                answer.append(i)
+            if len(answer)==2:
+                return answer
