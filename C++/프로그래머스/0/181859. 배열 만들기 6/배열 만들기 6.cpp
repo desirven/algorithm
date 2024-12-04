7c04766332793576c31e338c50a7475fabda111d
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-12-04 22:21:12 +0900

    [level 0] Title: 배열 만들기 6, Time: 9.65 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for (int i=0; i<arr.size(); i++){
+        if (answer.size()>0 && answer.back()==arr[i]) answer.pop_back();
+        else answer.emplace_back(arr[i]); 
+    }
+    if (answer.size()==0) answer.emplace_back(-1);
+    return answer;
+}
