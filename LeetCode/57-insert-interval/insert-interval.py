cd0b6bd3d780769e76a6d3b7fa2d2df781cbfe87
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-12-09 21:48:16 +0900

    Time: 0 ms (100.00%) | Memory: 19.5 MB (25.27%) - LeetSync

--- LeetCode/57-insert-interval/insert-interval.py
+++ LeetCode/57-insert-interval/insert-interval.py
@@ -1,24 +1,15 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        merged = []
+        ans = []
         i = 0
-        
-        while i < len(intervals) and intervals[i][1] < newInterval[0]:
-            merged.append(intervals[i])
+        while i < len(intervals) and newInterval[0] > intervals[i][1]:
+            ans.append(intervals[i])
+            i+=1
+        while i < len(intervals) and newInterval[1] >= intervals[i][0]:
+            l = min(intervals[i][0], newInterval[0])
+            r = max(intervals[i][1], newInterval[1])
+            newInterval[0] = l
+            newInterval[1] = r
             i += 1
-    
-        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            print(i)
-            print(newInterval[0])
-            print(newInterval[1])
-            i += 1
-    
-        merged.append(newInterval)
-    
-        while i < len(intervals):
-            merged.append(intervals[i])
-            i += 1
-    
-        return merged
+        ans.append([newInterval[0], newInterval[1]])
+        return ans + intervals[i:]
