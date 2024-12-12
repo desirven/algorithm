99a6e6ee456b9638cd2b52e67805f407b4a007fa
Lee SooHyung <132976008+desirven@users.noreply.github.com>
2024-12-12 20:38:51 +0900

    Time: 0 ms (100.00%) | Memory: 17.2 MB (15.90%) - LeetSync

--- LeetCode/71-simplify-path/simplify-path.py
+++ LeetCode/71-simplify-path/simplify-path.py
@@ -2,9 +2,8 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         answer = []
         for d in path.split("/"):
-            if d=="..":
-                if len(answer)>0:
-                    answer.pop()
-            elif d not in ["", "."]:
+            if d==".." and len(answer)>0:
+                answer.pop()
+            elif d not in ["", ".", ".."]:
                 answer.append(d)
         return "/" + "/".join(answer)
